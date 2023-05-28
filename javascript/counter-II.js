7c1304f3faba9adcd3437d1eda6fc97caf1e9805
GonzaCC <acanazac@uni.pe>
2023-05-28 13:51:12 -0500

    [Javascript] Ejercicio 08

---
+++ javascript/counter-II.js
@@ -0,0 +1,24 @@
+var createCounter = function(init) {
+    operations = {
+        value: init,
+        increment: function() {
+            this.value += 1
+            return this.value
+        },
+        decrement: function() {
+            this.value -= 1
+            return this.value
+        },
+        reset: function() {
+            this.value = init
+            return this.value
+        }
+    }
+    return operations
+};
+
+
+const counter = createCounter(5)
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
